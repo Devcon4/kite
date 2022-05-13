7b79a9c221ace367b38d43f583a9b4d8a392e4f2
Devyn Cyphers <devyn.cyphers@cen.centeva.com>
2022-05-13 12:45:32 -0600

    Create angular app with standalone syntax

---
+++ app/proxy.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'api/*': {
+    target: 'https://localhost:7998',
+    secure: false,
+  }
+}
---
+++ app/src/main.ts
@@ -0,0 +1,18 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+const routes: Routes = [];
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes)),
+  ]
+}).catch(err => console.error(err));

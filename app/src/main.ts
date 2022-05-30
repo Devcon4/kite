3522d6d403dcac19df13141099cdd08b69ad47da
Devyn Cyphers <devyncyphers@gmail.com>
2022-05-29 21:03:40 -0600

    Fix namespace; fix routes

--- app/src/main.ts
+++ app/src/main.ts
@@ -18,6 +18,11 @@ const routes: Routes = [
     redirectTo: 'launchpad',
     path: '',
   },
+  {
+    pathMatch: 'full',
+    redirectTo: 'launchpad',
+    path: '**',
+  },
 ];
 
 if (environment.production) {

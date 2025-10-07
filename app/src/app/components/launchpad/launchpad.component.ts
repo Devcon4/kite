9bdf525cf2d9148bc486e665bb9498f37cb9f628
Devyn Cyphers <devyncyphers@gmail.com>
2025-10-06 18:11:35 -0600

    update migration from 14 to 15

--- app/src/app/components/launchpad/launchpad.component.ts
+++ app/src/app/components/launchpad/launchpad.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
 import 'css-paint-polyfill';
 import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
